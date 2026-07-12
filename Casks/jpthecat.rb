3ec7abbafaa030c17eaad6173fc4f786d1a354e8
8BIT <1309181+8bittts@users.noreply.github.com>
2026-07-12 08:12:29 -0700

    Update JPtheCat v1.035

--- Casks/jpthecat.rb
+++ Casks/jpthecat.rb
@@ -1,6 +1,6 @@
 cask "jpthecat" do
-  version "1.034"
-  sha256 "aec959b3a6ee3abf2689278d83519f9c7c89e72b7ebfdf648f34fd72ec930006"
+  version "1.035"
+  sha256 "9eb9629d4cfb2fbdc27b7ab227e45bd46f1c1660826a20cb9fe18929a1d5653f"
 
   url "https://api.jpthecat.com/storage/v1/object/public/releases/JPtheCat-#{version}.dmg"
   name "JPtheCat"
